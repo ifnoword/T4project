4f5eb3fdfc1143c98c35327a7a4bbbd3395c9ed3
atsnyder <adam-snyder@uiowa.edu>
2013-10-31 14:48:53 -0700

    10-31 4:48 Adam

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 
-def new 
-end
+  def new 
+  end #new
+
+  def create
+    puts params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "#{@user.email} was successfully created."
+      redirect_to jobs_path
+    else
+      render 'new'
+    end
+  end #create
+
 end
