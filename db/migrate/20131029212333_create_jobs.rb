ee3ad36d75bce832615093c9b935d3f33f291181
ifnoword <xinmei-zhang@uiowa.edu>
2013-10-31 19:12:18 -0700

    9:12

--- db/migrate/20131029212333_create_jobs.rb
+++ db/migrate/20131029212333_create_jobs.rb
@@ -1,8 +1,12 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-
-      t.timestamps
+      t.string :employer #email of the user class
+      t.string :title #job title
+      t.text :desc #brief job description
+      t.text :reqs #job requirements
+      t.text :desire # desired but NOT required qualification
+      t.timestamps 
     end
   end
 end
