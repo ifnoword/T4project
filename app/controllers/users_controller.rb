fe60a1f8bf1d6ad0beed9443534fc579d94b54f7
saasbook <saasbook@saasbook.(none)>
2013-11-05 15:02:30 -0800

    presence validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
     @user = User.create_user!(params[:user])
 	
     if !!@user       
+      flash[:notice] = "Please enter correct information."
+      redirect_to new_user_path     
+    else
       flash[:notice] = "#{@user.email} was successfully created."
       redirect_to jobs_path
-    else
-	flash[:notice] = "Try again."
-      redirect_to new_user_path
     end
   end #create
 
