5808e54073cb0683694bdf7c9661d026e1e19189
ifnoword <xinmei-zhang@uiowa.edu>
2013-11-06 02:37:34 -0800

    validDone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131029215546) do
+ActiveRecord::Schema.define(:version => 20131106044842) do
 
   create_table "jobs", :force => true do |t|
     t.string   "employer"
@@ -26,9 +26,11 @@ ActiveRecord::Schema.define(:version => 20131029215546) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "name"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
